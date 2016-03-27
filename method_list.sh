11990e4ab12f1664e43c2b45d01a9712711742f9
Early Ehlinger <earlye@gmail.com>
2016-03-27 10:01:05 -0500

    Made method_list.sh work for linux and osx. Default is linux style.

--- method_list.sh
+++ method_list.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-nm -just-symbol-name -defined-only $1 | c++filt
+case "$(uname)" in
+    'Darwin') # OSX
+	nm -just-symbol-name -defined-only $1 | c++filt
+	;;
+
+    'Linux') # UBUNTU: Linux
+	nm -g --defined-only src/test/test_basic_component.o | sed -r "s/^.{19}//" | c++filt
+	;;
+
+    *) # DEFAULT
+	nm -g --defined-only src/test/test_basic_component.o | sed -r "s/^.{19}//" | c++filt
+	;;
+esac
+
 
